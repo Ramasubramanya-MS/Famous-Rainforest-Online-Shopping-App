7a7e01054e16a32223ef40f1f93527364b9b2d12
Ramasubramanya-MS <ramasubramanyams1729@gmail.com>
2025-04-05 01:27:10 +1100

    Add Category Crud Operations

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // How will we get connection string from appsettings.json
+        // Inject ApplicationDbContext / Configure ApplicationDbContext, we will get connectionstring as a parameter in constructor as DbContext option and we will be passing on to base class
+
+        public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options) : base (options)
+        {
+
+            
+        }
+        // EFcore automatically creates table in SQL Table
+        public DbSet<Category> Categories { get; set; }
+
+        // Overriding a model builder
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 11, Name = "Drama", DisplayOrder = 1 },
+                new Category { Id=12, Name="Booze", DisplayOrder=2}
+                );
+        }
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        // Using data annotation as the primary key
+        [Key]
+        public int Id { get; set; }
+        // Not Null Setting
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Range(1,100)]
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
